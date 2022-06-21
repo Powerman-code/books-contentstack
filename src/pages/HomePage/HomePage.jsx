5307a0f79d34470617efe19cb37e556a40956b92
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2022-06-21 19:06:39 +0300

    small fixes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,19 @@
 // import Gallery from "../../components/gallery/gallery";
 // import s from "./HomePage.module.scss"
+import contentstack from "contentstack";
+
+// const Stack = Contentstack.Stack(
+//   "<stack_api_key>",
+//   "<stack_delivery_token>",
+//   "<environment_name>"
+// );
+
+const Stack = contentstack.Stack({
+  api_key: process.env.development.local.REACT_APP_APIKEY,
+  delivery_token: process.env.development.local.REACT_APP_DELIVERY_TOKEN,
+  environment: process.env.development.local.REACT_APP_ENVIRONMENT,
+  region: process.env.development.local.REACT_APP_REGION,
+});
 
 const HomePage = () => {
   return <div>HOME PAGE</div>;
